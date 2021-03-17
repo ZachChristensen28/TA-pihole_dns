3d3c56f82767487e9a03b15ea22df9008e21b123
Zach Christensen <zach.christensen@kinneygroup.com>
2021-03-16 21:33:26 -0600

    updated api interaction

--- bin/input_module_pihole_system.py
+++ bin/input_module_pihole_system.py
@@ -1,22 +1,18 @@
+
 # encoding = utf-8
 
-import os
-import sys
-import time
-import datetime
-import json
+from pihole_helper import *
 import pihole_constants as const
 
 
 def validate_input(helper, definition):
    # We have nothing left to verify
-        pass
+    pass
 
 
 def collect_events(helper, ew):
     # Get Credentials
-    account = helper.get_arg('account')
-    api_key = account['api_key']
+    account = helper.get_arg('pihole_account')
     pihole_host = account['pihole_host']
 
     # Get Log Level
@@ -24,87 +20,32 @@ def collect_events(helper, ew):
     helper.set_log_level(log_level)
     helper.log_info(f'log_level="{log_level}"')
 
-    # Get Interval
-    interval = int(helper.get_arg('interval'))
-
-    # Get Checkpoint
-    key = f'pihole_api_{pihole_host}'
-    current_time = int(time.time())
-    check_time = current_time - interval + 60
-
-    if helper.get_check_point(key):
-        old_state = helper.get_check_point(key)
-        helper.log_info(f'msg="Checkpoint found", hostname="{pihole_host}"')
-        helper.log_debug(
-            f'msg="Checkpoint information", checkpoint="{old_state}", interval="{interval}", hostname="{pihole_host}"')
-
-        if check_time < old_state:
-            helper.log_info(
-                f'msg="Skipping because interval is too close to previous run", action="aborted", hostname="{pihole_host}"')
-            return
-        else:
-            helper.log_info(f'msg="Running scheduled Interval", hostname="{pihole_host}"')
-
-    else:
-        helper.log_info(f'msg="Checkpoint file not found", hostname="{pihole_host}"')
-
-    # Get Proxy
-    proxy = helper.get_proxy()
-
-    if proxy:
-        if proxy["proxy_username"]:
-            helper.log_info('msg="Proxy is configured with authenticaiton"')
-            helper.log_debug(
-                f'proxy_type="{proxy["proxy_type"]}", proxy_url="{proxy["proxy_url"]}", proxy_port="{proxy["proxy_port"]}", proxy_username="{proxy["proxy_username"]}"')
-        else:
-            helper.log_info('msg="Proxy is configured with no authentication"')
-            helper.log_debug(
-                f'proxy_type="{proxy["proxy_type"]}", proxy_url="{proxy["proxy_url"]}", proxy_port="{proxy["proxy_port"]}"')
-
-    # URL
-    url = f'http://{pihole_host}/{const.api_system}'
-    headers = {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
-    }
+    # Start ..
     params = {
         'summary': '',
         'type': '',
         'version': ''
     }
+    event_name = 'system_summary'
+    response = sendit(pihole_host, event_name, helper, params)
 
-    # Make Call
-    try:
-        helper.log_info(f'msg="starting request", action="starting", hostname="{pihole_host}"')
-        r = helper.send_http_request(
-            url, 'get', headers=headers, parameters=params, use_proxy=True)
-    except Exception as e:
-        helper.log_error(
-            f'error_msg="Unable to complete request", action="failed", hostname="{pihole_host}"')
-        helper.log_debug(f'error_msg="{e}"')
+    if not response:
         return False
 
-    if r.status_code == 200:
-        helper.log_info(f'msg="request completed", action="success", hostname="{pihole_host}"')
-        response = r.json()
-        event = {}
-        event['status'] = response['status']
-        event['privacy_level'] = response['privacy_level']
-        event['domains_on_blocklist'] = response['domains_being_blocked']
-        event['gravity_updated'] = response['gravity_last_updated']['absolute']
+    # Build Event
+    event = {}
+    event['status'] = response['status']
+    event['type'] = response['type']
+    event['version'] = response['version']
+    event['privacy_level'] = response['privacy_level']
+    event['domains_on_blocklist'] = response['domains_being_blocked']
+    event['unique_domains'] = response['unique_domains']
+    event['gravity_updated'] = response['gravity_last_updated']['absolute']
 
-        # Create Splunk Event
-        splunk_event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(
-        ), sourcetype=helper.get_sourcetype(), data=json.dumps(event), host=pihole_host)
-        ew.write_event(splunk_event)
+    # Create Splunk Event
+    splunk_event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(
+    ), sourcetype=helper.get_sourcetype(), data=json.dumps(event), host=pihole_host)
+    ew.write_event(splunk_event)
 
-        # Checkpointer
-        new_state = int(time.time())
-        helper.save_check_point(key, new_state)
-        helper.log_info(
-            f'msg="Updating Checkpoint", checkpoint="{new_state}", hostname="{pihole_host}"')
-    else:
-        helper.log_error(
-            f'error_msg="Unable to retrieve information", action="failed", hostname="{pihole_host}"')
-        helper.log_debug(f'status_code="{r.status_code}"')
-        return False
+    # Checkpointer
+    checkpinter(pihole_host, event_name, helper, set_checkpoint=True)
