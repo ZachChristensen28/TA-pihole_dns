611e1720582192d65ded7f2d7b65be6f291e9f9d
Zach Christensen <23529374+ZachChristensen28@users.noreply.github.com>
2021-06-04 20:56:06 -0600

    Removing legacy API configs

--- bin/pihole_constants.py
+++ bin/pihole_constants.py
@@ -4,11 +4,7 @@ h_proto = 'http'
 # port
 p_port = '8080'
 
-# Filters
-filters = ['white', 'regex_white', 'black', 'regex_black']
-
 # endpoints
-api_system = 'admin/api.php'
 api_domain = 'api/domains'
 api_status = 'api/dns/blocking'
 api_cache = 'api/dns/cacheinfo'
